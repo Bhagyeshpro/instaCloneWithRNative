bbf80c3949681ddd1ac22d966b0795158c882114
Bhagyeshpro <bhagyeshpaunikar2001@gmail.com>
2021-11-14 13:00:10 +0530

    Staring My Own LoginScreen

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Insta Clone!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ Components/Auth/Landing.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button, View } from "react-native";
+
+export default function Landing() {
+  return (
+    <View>
+      <Button title="Register" />
+    </View>
+  );
+}
