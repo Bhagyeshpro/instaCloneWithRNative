8649cbf4b69197bafc1a882261f9ce83044ea68c
Bhagyeshpro <bhagyeshpaunikar2001@gmail.com>
2021-11-19 15:25:48 +0530

    Pushing Dev Branch

--- Components/Auth/Landing.js
+++ Components/Auth/Landing.js
@@ -4,7 +4,7 @@ import { Button, Image, StyleSheet, Text, View } from "react-native";
 export default function Landing({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={styles.text}>WELCOME TO INSTAGRAM</Text>
+      <Text style={styles.text}>WELCOME TO INSTAGRAM By Me</Text>
       <Image
         source={
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRP5DzZgTLz3rtU2PgabPYawok_tw_0BO03bljed246ps_bDbi--7ucC-uUUu4dj_Q2tQ&usqp=CAU"
