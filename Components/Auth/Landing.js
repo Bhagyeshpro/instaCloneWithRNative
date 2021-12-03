c5d349a5045b98c7f88dafff8de27a23f6850903
Bhagyeshpro <bhagyeshpaunikar2001@gmail.com>
2021-12-03 13:35:05 +0530

    Working

--- Components/Auth/Landing.js
+++ Components/Auth/Landing.js
@@ -10,17 +10,19 @@ export default function Landing({ navigation }) {
           "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRP5DzZgTLz3rtU2PgabPYawok_tw_0BO03bljed246ps_bDbi--7ucC-uUUu4dj_Q2tQ&usqp=CAU"
         }
         style={styles.image}
-    <View
-      style={[
-        styles.container,
-        { flex: 1, alignItems: "center", justifyContent: "center" },
-      ]}
-    >
-      <Button title="Log In" onPress={() => navigation.navigate("Login")} />
-      <Button
-        title="Register"
-        onPress={() => navigation.navigate("Register")}
       />
+      <View
+        style={[
+          styles.container,
+          { flex: 1, alignItems: "center", justifyContent: "center" },
+        ]}
+      >
+        <Button title="Log In" onPress={() => navigation.navigate("Login")} />
+        <Button
+          title="Register"
+          onPress={() => navigation.navigate("Register")}
+        />
+      </View>
     </View>
   );
 }
